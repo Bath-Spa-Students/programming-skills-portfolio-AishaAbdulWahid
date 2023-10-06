db7ebe4023ebbc0bdf3e782f29df13db4fa59a5a
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-10-06 19:03:06 +0400

    Practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice5.py
@@ -0,0 +1,11 @@
+'''Q5. Write four string in different variables and 
+       print the output as one string (string concatenation)'''
+# Define a four string variables
+str1 = "Why dont't some fish play paino?"
+str2 = "Because"
+str3 = "you can't tuna"
+str4 = "fish!"
+
+# Cancatenate the string and print the result
+result = str1 + " " + str2 + " " + str3 + " " + str4
+print(result)
