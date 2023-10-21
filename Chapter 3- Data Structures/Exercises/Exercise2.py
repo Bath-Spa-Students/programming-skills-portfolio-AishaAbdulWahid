448cbeee4d6bcb01b803b7c28e0f1332f2d53de6
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-10-21 16:18:36 +0400

    Exercise2.py Personalizing messages for a list of Names.

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,12 @@
+'''Start with the list you used in Exercise 1, but instead of just
+printing each person`s name, print a message to them. The text of each message should be the same,\
+      but each message should be 
+personalized with the person`s name.'''
+
+# List of friends names
+names = ["Aisha", "Urooj", "Suriya", "Zainab"]
+
+# Print personalized message for each name
+for name in names:
+    print(f"Salutations {name}!, My fellow Harry Potter enthusiast. Don`t forget to bring a copy of \
+'Hogwarts: A History' to our next book club meeting!.")
