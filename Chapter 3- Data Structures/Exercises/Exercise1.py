3eea63aa7b3d98bc967edca472e1639d3255f483
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-10-21 15:41:06 +0400

    Revert "Exercise3.py Printing names from a List."
    This reverts commit 4d2c37d4adc0c0a03cf507e296bc20fc1b4d2d88.

--- Chapter 3- Data Structures/Exercises/Exercise1.py
+++
@@ -1,9 +0,0 @@
-'''Store the names of a few of your friends in a list called names./
- Print each person`s name by accessing each element in the list, one at a time.'''
-
-# List of friends names
-names = ["Aisha", "Urooj", "Suriya", "Zainab"]
-
-# Print each name
-for name in names:
-    print(name)
