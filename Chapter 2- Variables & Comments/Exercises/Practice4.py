25127cdc4a73432572ac179cefabdd3ee85654a1
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-05 19:28:50 +0400

    Practice4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice4.py
@@ -0,0 +1,10 @@
+'''Write a python program that stores an integer and string value to variables x and y.\
+   Print the type of variable x and y.'''
+
+# Store an integer and string value to variables x and y.
+x = 1989
+y = "Hello"
+
+# Print the type of variable x and y.
+print(f"The type of variable x:", type(x))   # Alternate line of code print(f"The type of variable x is {type(x)}.")
+print(f"The type of variable y:", type(y))   # Alternate line of code print(f"The type of variabel y is {type(y)}.")
