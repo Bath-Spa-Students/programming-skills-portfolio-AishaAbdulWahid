d319806c58fb9fc1bc405cae022bcfcfef83f2ff
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 16:42:41 +0400

    Practice2.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice2.py
@@ -0,0 +1,13 @@
+# Q2. Write a Python program to iterate through the keys of a dictionary and print them.
+ # Create a dictionary to store information
+my_self = {
+    'name' : 'Aisha',
+    'Age' : '18',
+    'Occupation' : 'Cyber-security analyst',
+    'Hobbies' : ['Reading', 'Going for a Run', 'Playing basketball', 'Coding'],
+    'Location' : 'United Arab Emirates'
+          }
+
+# Iterate through the keys 
+for key in my_self.keys():
+    print(key)
