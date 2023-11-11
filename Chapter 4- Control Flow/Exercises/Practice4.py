dcb4eadfbd87ff066f20102e22dc12788d5d58a0
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 16:39:07 +0400

    Practice4.py

---
+++ Chapter 4- Control Flow/Exercises/Practice4.py
@@ -0,0 +1,12 @@
+'''Q4. Write a python program with an if-else statement that displays Speed is normal if the speed 
+       variable is within the range of 24 to 56. If the speed variable's value is outside this range, display 
+      'Speed is abnormal'.'''
+
+# 'speed' is the variable
+speed = 30
+
+# Using an if-else statement to check the value of 'speed'
+if 24 <= speed <= 56:
+    print("Speed is normal.")
+else:
+    print("Speed id abnormal.")
