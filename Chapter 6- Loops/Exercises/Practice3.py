38cbc58e8efa8e317c24740a5f352e521c19fc1c
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 17:28:54 +0400

    Practice3.py

---
+++ Chapter 6- Loops/Exercises/Practice3.py
@@ -0,0 +1,15 @@
+# Q3. Write a Python program that uses a loop to find the sum of all the numbers from 1 to 100.
+
+# Initialize the sum
+total = 0
+
+# Using a for loop to iterate over the numbers from 1 to 100
+for number in range (1, 101):
+    # Add the current current number to the total
+    total += number
+
+    # Print the total
+    print(total)
+'''The sum of all the numbers from 1 to 100 is 5050 due to a mathematical formula for the sum of an mathemmatical formula\
+   for the sum of an arithmetic series. This is the formula: n/2 * (a + 1).\
+   n = no. of terms, a = first term, l = is the last term.'''
