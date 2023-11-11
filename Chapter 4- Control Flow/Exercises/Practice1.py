63687b15e6c9c4c66fa4dd5978ac7a92e6f48990
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 16:34:32 +0400

    Practice1.py

---
+++ Chapter 4- Control Flow/Exercises/Practice1.py
@@ -0,0 +1,16 @@
+'''Q1. Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the 
+variable z if the variable '&' (ampersand) is greater than 100.'''
+
+# Assuming '&' is your variable.
+ampersand = 150 
+
+# Using an if statement to check if '&' is greater than 100.
+if ampersand > 100: 
+    # If true, assign 20 to 'y' and 40 to 'z'.
+    y = 20
+    z = 40
+    
+    # If condition true, we assign the value 20 to the variable 'y'.
+    print(y)
+    # And assign the value 40 to the variable 'z'.
+    print(z)
