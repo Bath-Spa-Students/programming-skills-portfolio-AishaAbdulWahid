a00c5e4fa099b0eb63bded9341fefcef87285102
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 16:42:24 +0400

    Practice1.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice1.py
@@ -0,0 +1,14 @@
+# Q1. Use a dictionary to store information about yourself.
+
+# Create a dictionary to store information
+my_self = {
+    'name' : 'Aisha',
+    'Age' : '18',
+    'Occupation' : 'Cyber-security analyst',
+    'Hobbies' : ['Reading', 'Going for a Run', 'Playing basketball', 'Coding'],
+    'Location' : 'United Arab Emirates'
+          }
+
+# Print the dictionary
+print(my_self)
+    
