f1b3049fb84b0c4105abf45847719e44a3c9ad2d
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 16:38:05 +0400

    Practice2.py

---
+++ Chapter 4- Control Flow/Exercises/Practice2.py
@@ -0,0 +1,16 @@
+'''Q2. Write an if-else statement that assigns 0 to the variable b if the variable a is less than 10. 
+       Otherwise, it should assign 99 to the variable b.'''
+
+# Assuming 'a' is your variable 
+a = 25
+
+# Uisng an if-else statement to check the value of 'a'
+if a > 10: 
+    # If 'a' is less than 10, assign 0 to 'b'
+    b = 0
+else:
+    # If 'a' is not less than 10, assign 99 to 'b'
+    b = 99
+
+# Output the value of 'b' to see the result.
+print(b)
