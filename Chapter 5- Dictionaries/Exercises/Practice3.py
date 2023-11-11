c47b52a6044a519d83562b49129203d49320040c
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 16:42:58 +0400

    Practice3.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice3.py
@@ -0,0 +1,13 @@
+# Q3. Write a Python program to iterate through the values of a dictionary and print them.
+# Create a dictionary to store information
+my_self = {
+    'name' : 'Aisha',
+    'Age' : '18',
+    'Occupation' : 'Cyber-security analyst',
+    'Hobbies' : ['Reading', 'Going for a Run', 'Playing basketball', 'Coding'],
+    'Location' : 'United Arab Emirates'
+          }
+
+# Iterate through the values
+for value in my_self.values():
+    print(value) 
