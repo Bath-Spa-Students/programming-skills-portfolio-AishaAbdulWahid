b121a5378bd7d2d9822acb825d903571962e3c38
AishaAbdulWahid <144208786+AishaAbdulWahid@users.noreply.github.com>
2023-11-11 16:12:49 +0400

    Delete Chapter 3- Data Structures/Exercises/Practice2.py

--- Chapter 3- Data Structures/Exercises/Practice2.py
+++
@@ -1,4 +0,0 @@
-'''Q2. Write a statement that creates a list with the following strings:\
-      'Einstein', Newton' , 'Copernicus', and 'Kepler' ''' 
-
-scientists = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
