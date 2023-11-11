63b8faed98a1c89aeff62fb450ece2d605807210
AishaAbdulWahid <aisha.a.w.2005@gmail.com>
2023-11-11 16:14:25 +0400

    Practice2.py

---
+++ Chapter 3- Data Structures/Exercises/Practice2.py
@@ -0,0 +1,9 @@
+"""Q2. Write a statement that creates a list with the following strings:\
+      'Einstein', Newton' , 'Copernicus', and 'Kepler' """
+
+# create a list named scientists with the following strings
+scientists = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
+# Use a for loop to iterate over each element in the list
+for scientist in scientists:
+    # Print the current element
+    print(scientist)
